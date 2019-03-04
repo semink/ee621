65cb35a50a653517543c4b6e192a739d9fcac697
Semin Kwak <semin.kwak@epfl.ch>
2019-03-04 10:04:17 +0100

    submmit

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 close all;
-rng(1);
-% Douglas-Rachford algorithm
+rng(3);
+%% Douglas-Rachford algorithm
 addpath("modules")
 n = 200;
 p = n/4;
@@ -14,38 +14,49 @@ pA = A'*(A*A')^(-1);
 ProxE = @(x)x - pA*(A*x - y);
 
 options.report = @(x)struct('q', q(x));
-options.alpha = 1;  % regularization parameter
+options.alpha = 0.6;  % regularization parameter
 options.mu = 0.001; % step size
-options.niter = 2000;   % number of iteration 
+options.niter = 4000;   % number of iteration 
 
+% run algorithm
 [x, R] = perform_dr(zeros(n,1), Proxq, ProxE, options);
 
 Q = s2v(R,'q');
+
+
+% Plot
 figure;
 semilogy(Q(2:end)-min(Q(2:end)), 'k', 'LineWidth', 2);
 xlabel("iteration index n")
 ylabel("q(w_n)-q(w_n^*)")
 axis tight; grid on; title('Learning curve');
-% Subgradient learning algorithm
+
+
+%% Subgradient learning algorithm
 
 
 sP = @(w, alpha)(alpha*sign(w)+2*A'*(A*w-y)/length(y));
-
 q = @(w)norm(w,1);
 E = @(w)norm(y-A*w)^2/length(y);
 P = @(w, alpha) alpha*q(w) + E(w);
 
+% parameters
 sub_options.report = @(w, alpha)struct('P', P(w, alpha));
 sub_options.mu = 0.001;
-sub_options.niter = 2000;
+sub_options.niter = 4000;
 sub_options.alpha = 0.25;
 sub_options.kappa = 0.3;
+
+% run algorithm with smoothing
 [w_smooth, R_smooth] = perform_subgradient(zeros(n,1), sP, sub_options);
 p_smooth = s2v(R_smooth,'P');
 sub_options.kappa = 0;
+
+% run algorithm without smoothing
 [w, R] = perform_subgradient(zeros(n,1), sP, sub_options);
 p = s2v(R,'P');
 
+% plot
 figure,
 semilogy(p-min(p_smooth), 'r--', 'LineWidth', 2)
 hold on
